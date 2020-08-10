a213fa0e573d718a19a45c1e8d6929aa495c7083
joshkelly <josh.d.kelly@gmail.com>
2020-08-10 11:10:57 -0700

    added user data

--- basic_deploy.tf
+++ basic_deploy.tf
@@ -6,8 +6,13 @@ locals {
   project_id = var.project_id
 }
 
+data "template_file" "user_data"{
+  template = file("user_data.txt")
+}
+
 resource "packet_device" "web1" {
   hostname         = "web1"
+  user_data        = data.template_file.user_data.rendered
   plan             = "t1.small.x86"
   facilities       = ["ewr1"]
   operating_system = "ubuntu_18_04"
